e9989e3f3aadc95b7489fb429fca77fdc83f75be
Joe.Hediger <joseph.hediger@students.tamuk.edu>
2023-10-31 22:30:55 -0500

    corrected error in scaled rainfall data

--- scripts/phi.rainfall.R
+++ scripts/phi.rainfall.R
@@ -21,7 +21,7 @@ data2 <- data[!data$animal_id %in% to_remove$animal_id,]
 ch<- pivot_wider(data2, names_from = 'year', values_from = 'status', id_cols = 'animal_id' )
 ch<-ch[,-1]
 ch<-as.matrix(ch)
-
+data2$annual <-scale(data2$annual)
 annual.rainfall<-pivot_wider(data2, names_from = 'year', values_from = 'annual', id_cols = 'animal_id' )
 annual.rainfall<-annual.rainfall[,-1]
 annual.rainfall<-as.matrix(annual.rainfall)
@@ -56,8 +56,6 @@ birthyear <- as.numeric(as.factor(id.bs.by$birth_year))
 #create capture year vector
 capyear <- f
 
-#scale and center covariate annual rainfall
-annual.rainfall<-scale(annual.rainfall)
 # 
 # create vector with last occasion for each individual, marked by 2, 15 for end of study
 # rework h to only include capture myopathy or harvest, do not censor natural mortality
@@ -150,67 +148,3 @@ cjs.age.site <- jagsUI(jags.data, inits, parameters, "cjs-age-site.jags", n.chai
                        n.thin = nt, n.iter = ni, n.burnin = nb, parallel = FALSE)
 
 print(cjs.age.site)
-
-# 
-# # Specify model in JAGS language
-# sink("cjs.jags")
-# cat("
-# model {
-# 
-# #prior for recapture prob
-# p ~ dbeta(1, 1)
-#    
-# 
-# #priors
-# phi ~ dbeta(1,1)
-# 
-# 
-# # Likelihood 
-# for (i in 1:nind){
-#    # Define latent state at first capture, we know for sure the animal is alive
-#       z[i,f[i]] <- 1
-#       
-#       for (t in (f[i]+1):h[i]){ 
-#         # State process
-#             z[i,t] ~ dbern(mu1[i,t]) #toss of a coin whether individual is alive or not detected 
-#             mu1[i,t] <- phi[i,t-1] * z[i,t-1]  #t-1 because we are looking ahead to see if they survived from 1 to 2 based upon them being alive at 2
-# 
-#         # Observation process
-#             ch[i,t] ~ dbern(mu2[i,t])
-#             mu2[i,t] <- p * z[i,t]
-#       } #t
-#    } #i
-# 
-# 
-# }
-# ",fill = TRUE)
-# sink()
-# 
-# 
-# #Function for latent state
-# z.init <- matrix(NA, nrow = nrow(ch), ncol = ncol(ch))
-# 
-# for(i in 1:dim(z.init)[1]){
-#   z.init[i, f[i]:h[i]] <- 1
-#   z.init[i,f[i]] <- NA
-# }
-# 
-# 
-# # Bundle data
-# jags.data <- list(ch = ch, f = f, h = h, nind = nrow(ch))#, 
-# 
-# # Initial values
-# inits <- function(){list(z = z.init, phi = rbeta(1,1,1), p = rbeta(1,1,1))} #
-# 
-# parameters <- c('mu1','p')
-# # 'survival', 'site_diff' 'survival',, 'site_diff','eps.capyear' 'int','site.beta',
-# 
-# # MCMC settings
-# ni <- 1000
-# nt <- 1
-# nb <- 500
-# nc <- 3
-# 
-# # Call JAGS from R (BRT 3 min)
-# cjs.age.site <- jagsUI(jags.data, inits, parameters, "cjs.jags", n.chains = nc, 
-#                        n.thin = nt, n.iter = ni, n.burnin = nb, parallel = FALSE)

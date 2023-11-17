5d43e97a9f62698bd987fbac7359b06752508f5b
Joe.Hediger <joseph.hediger@students.tamuk.edu>
2023-11-16 18:41:38 -0600

    created file with rainfall january to december and one with rainfall november to october

--- scripts/rainfall.R
+++ scripts/rainfall.R
@@ -13,36 +13,36 @@ west.rain <- rename(west.rain, rain=WEST.YANA) #renaming column to rain
 east.rain$site<- 'ey'
 east.rain <- rename(east.rain, rain=EAST.YANA)
 data1<- rbind(west.rain,east.rain)
-data1$surv.year <- as.factor(data1$surv.year)
+data1$surv.year <- as.factor(data1$Year)
 data1$site <-as.factor(data1$site)
 cor(west.rain$rain, east.rain$rain) #92% correlation between east and west yana
 
-totalrain<- data1 %>% group_by(surv.year, site ) %>% summarise(total.rain=sum(rain)) #sum is total annual rainfall from nov to oct
-totalrain<- totalrain[-c(43:44),]#remove 2022
+totalrain<- data1 %>% group_by(Year, site ) %>% summarise(total.rain=sum(rain)) #sum is total annual rainfall from nov to oct
+# totalrain<- totalrain[-c(43:44),]#remove 2022
 
 #filter for only march april and may rainfall
 march.apr.may <- data1 %>%
   filter(Month %in% c('Mar', 'Apr', 'May'))
 #group by year to sum march april and may rainfall
-total.march.apr.may<- march.apr.may %>% group_by(surv.year,site) %>% summarise(sum.march.apr.may=sum(rain))
+total.march.apr.may<- march.apr.may %>% group_by(Year,site) %>% summarise(sum.march.apr.may=sum(rain))
   
 #filter for only june july aug rainfall
 jun.jul.aug <- data1 %>%
   filter(Month %in% c('Jun', 'Jul', 'Aug'))
 
 #group by year to sum march april and may rainfall
-total.jun.jul.aug<- jun.jul.aug %>% group_by(surv.year,site) %>% summarise(sum.jun.jul.aug=sum(rain))
+total.jun.jul.aug<- jun.jul.aug %>% group_by(Year,site) %>% summarise(sum.jun.jul.aug=sum(rain))
 
 #filter for only march april and may rainfall
 sept.oct <- data1 %>%
   filter(Month %in% c('Sept', 'Oct'))
 #group by year to sum march april and may rainfall
-total.sept.oct<- sept.oct %>% group_by(surv.year,site) %>% summarise(sum.sept.oct=sum(rain))
+total.sept.oct<- sept.oct %>% group_by(Year,site) %>% summarise(sum.sept.oct=sum(rain))
 #filter for only march april and may rainfall
 oct <- data1 %>%
   filter(Month %in% c('Oct'))
 #group by year to sum march april and may rainfall
-total.oct<- oct %>% group_by(surv.year,site)
+total.oct<- oct %>% group_by(Year,site)
 total.oct <- rename(total.oct, sum.oct = rain)
 
 rainfall<- total.jun.jul.aug
@@ -50,6 +50,6 @@ rainfall$sum.march.apr.may <- total.march.apr.may$sum.march.apr.may
 rainfall$sum.sept.oct <- total.sept.oct$sum.sept.oct
 rainfall$oct <- total.oct$sum.oct
 rainfall$annual<- totalrain$total.rain
-rainfall <- rainfall[,-4]
+# rainfall <- rainfall[,-4]
 
-write.csv(rainfall, './cleaned/rainfall_clean1.csv', row.names = F)
+write.csv(rainfall, './cleaned/rainfall_clean_jan_dec.csv', row.names = F)

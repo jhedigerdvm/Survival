51effef9ada5f27f5309ff08d12843d0c16f3ef8
Joe.Hediger <joseph.hediger@students.tamuk.edu>
2024-03-12 12:56:41 -0500

    updated models and updated figure

--- scripts/finale.R
+++ scripts/finale.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b499cc74153a64c472404cd1711dcaca6832735830801a9acde586a304d68c1
-size 22269
+oid sha256:c1c1804f2aee10180e05c5a760c900d0dc40751343fd168ca72e55bcea0b7843
+size 25581

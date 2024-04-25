df75519ac22a6c20a97ebdb34f08ff073e7f6b0a
Joe.Hediger <joseph.hediger@students.tamuk.edu>
2024-04-25 12:46:37 -0500

    subsetted ch to 3 individuals, error in node z[1,3]

--- scripts/dummycode.weight.lp.R
+++ scripts/dummycode.weight.lp.R
@@ -15,6 +15,8 @@ ch<-ch[,-1]
 ch<-as.matrix(ch)
 dim(ch)
 
+ch <- ch[c(1:3),]
+
 # Create vector with the occasion each indiv is marked, this gets weird because we know each individual was caught
 #at birth, but we are starting at the second capture occasion
 get.first <- function(x) min(which(x!=0)) #x! identifies when x is not equal to zero
@@ -23,7 +25,7 @@ f <- apply(ch, 1, get.first)
 
 #add weight and antler vectors
 weight<- pivot_wider(data, names_from = 'year', values_from = 'weight', id_cols = 'animal_id' )
-weight<- as.matrix(weight[,-1])
+weight<- as.matrix(weight[c(1:3),-1])
 
 
 #Function for latent state
@@ -45,7 +47,7 @@ occasions <- rowSums(is.na(weight)) # number of NA occasions for individual
 # weight <- as.data.frame(weight)
 indices <- as.data.frame(which(is.na(weight), arr.ind=T))
 indices <- indices %>% arrange(row) %>%  group_by(row) %>%  mutate(n=1:n()) %>% ungroup() #arrange orders the "rows" , group by may not be necessary, mutate creates a new column inserting the number for that individual 
-NA_indices <- matrix(NA, nrow=489, ncol=15)
+NA_indices <- matrix(NA, nrow=3, ncol=15)
 for(i in 1:nrow(indices)){
   NA_indices[indices[[i,1]],indices[[i,3]]] <- indices[[i,2]]
 }
@@ -60,22 +62,22 @@ for(i in 1:nrow(indices)){
 # NA_indices <- NA_indices[,-1]
 # NA_indices <- as.matrix(NA_indices)
 
-
-known.fate <- ch #known fate matrix with 2 indentifying deaths associated with capture or harvest
-
-#create capture history with just 1s and 0s, remove 'known fates'
-indices <- which(ch == 2, arr.ind = TRUE) #34 individuals with known fates
-ch[indices] <- 1
-
-
 # 
-# create vector with last occasion for each individual, marked by 2, 15 for end of study
-# rework h to only include capture myopathy or harvest, do not censor natural mortality
-get.last<- function(x) min(which(x>1))
-h <- apply(known.fate,1,get.last)
-h <- replace(h, is.infinite(h), 15)
-h
-f-h #check for zero
+# known.fate <- ch #known fate matrix with 2 indentifying deaths associated with capture or harvest
+# 
+# #create capture history with just 1s and 0s, remove 'known fates'
+# indices <- which(ch == 2, arr.ind = TRUE) #34 individuals with known fates
+# ch[indices] <- 1
+# 
+# 
+# # 
+# # create vector with last occasion for each individual, marked by 2, 15 for end of study
+# # rework h to only include capture myopathy or harvest, do not censor natural mortality
+# get.last<- function(x) min(which(x>1))
+# h <- apply(known.fate,1,get.last)
+# h <- replace(h, is.infinite(h), 15)
+# h
+# f-h #check for zero
 
 
 #####################################33
@@ -89,7 +91,7 @@ p ~ dbeta(1, 1)
 
 #priors
 
-for (u in 1:489){                 #for u individuals 1 to 489
+for (u in 1:3){                 #for u individuals 1 to 489
   for (j in 1:occasions[u]){  #for number of NA occasions for individual u
   weight[u,NA_indices[u,j]] ~ dnorm(0,0.0001)         #in weight.init, row u, column with NA, this code is just trying to find the column with NAs
      }
@@ -103,11 +105,11 @@ sigma ~ dunif(0,100)
 
 
 # Likelihood
-for (i in 1:nind){
+for (i in 1:3){
    # Define latent state at first capture, we know for sure the animal is alive
       z[i,f[i]] <- 1
 
-      for (t in (f[i]+1):h[i]){
+      for (t in (f[i]+1):15){
         # State process
             z[i,t] ~ dbern(mu1[i,t]) #toss of a coin whether individual is alive or not detected
             logit(phi[i,t-1]) <- weight.beta*weight[i,t-1] 
@@ -129,8 +131,8 @@ sink()
 
 
 # Bundle data
-jags.data <- list(ch = ch, f = f, h = h, nind = nrow(ch), nocc = ncol(ch), weight = weight, 
-                  occasions=occasions, NA_indices=NA_indices)#capyear=capyear, birthyear = birthyear  bs = bs,weight.sim = weight.sim,ageclass = ageclass
+jags.data <- list(ch = ch, f = f,  weight = weight, 
+                  occasions=occasions, NA_indices=NA_indices)#nind = nrow(ch), nocc = ncol(ch), h = h,capyear=capyear, birthyear = birthyear  bs = bs,weight.sim = weight.sim,ageclass = ageclass
 
 # Initial values
 inits <- function(){list(weight = weight.init, weight.beta = rnorm(1,0,10), z=z.init)} #, z.init
@@ -139,12 +141,12 @@ parameters <- c('weight.beta')#'int', 'age.beta', , 'p','survival'
 
 # MCMC settings
 ni <- 3000
-nt <- 1
+nt <- 10
 nb <- 1000
 nc <- 3
 
 # Call JAGS from R (BRT 3 min)
 cjs.weight <- jagsUI(jags.data, inits, parameters, "cjs-weight.jags", n.chains = nc,
-                     n.thin = nt, n.iter = ni, n.burnin = nb, parallel = FALSE)
+                     n.thin = nt, n.iter = ni, n.burnin = nb, parallel = T)
 
 print(cjs.weight)
